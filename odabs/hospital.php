eb65cb185f762be1d22a4a8b81b982e73ca173a8
Md. Ariful Haque <70318840+Arifulhaque313@users.noreply.github.com>
2021-11-18 20:45:59 +0600

    Add files via upload

---
+++ odabs/hospital.php
@@ -0,0 +1,119 @@
+<?php 
+  include "includes/header.php"
+?>
+
+
+
+<section class="container-fluid bg-light py-3">
+
+
+<div class= "container my-4 ">
+<h1 class="text-center " style="color:#089D49;">Hospitals list</h1>
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card">
+      <img src="img/h1.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Seikh fozilatunnesa Hospital </h5>
+       
+        <p class="card-text"><span class="las la-envelope"></span> <span><b>Email : </b>Fozilatunnesa123@gmail.com</span></p>
+        <p class="card-text"><span class="las la-phone"></span> <span><b>Mobile : </b>018--------,017--------</span></p>
+       <p class="card-text"><span class="las la-location-arrow"></span> <span><b>Address : </b>Cokroborti, Jirani, Savar</span></p>
+
+        <button class="btn btn-primary">View doctors</button>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="col">
+    <div class="card">
+      <img src="img/h1.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Seikh fozilatunnesa Hospital </h5>
+       
+        <p class="card-text"><span class="las la-envelope"></span> <span><b>Email : </b>Fozilatunnesa123@gmail.com</span></p>
+        <p class="card-text"><span class="las la-phone"></span> <span><b>Mobile : </b>018--------,017--------</span></p>
+       <p class="card-text"><span class="las la-location-arrow"></span> <span><b>Address : </b>Cokroborti, Jirani, Savar</span></p>
+
+        <button class="btn btn-primary">View doctors</button>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="img/h1.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Seikh fozilatunnesa Hospital </h5>
+       
+        <p class="card-text"><span class="las la-envelope"></span> <span><b>Email : </b>Fozilatunnesa123@gmail.com</span></p>
+        <p class="card-text"><span class="las la-phone"></span> <span><b>Mobile : </b>018--------,017--------</span></p>
+       <p class="card-text"><span class="las la-location-arrow"></span> <span><b>Address : </b>Cokroborti, Jirani, Savar</span></p>
+
+        <button class="btn btn-primary">View doctors</button>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="img/h1.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Seikh fozilatunnesa Hospital </h5>
+       
+        <p class="card-text"><span class="las la-envelope"></span> <span><b>Email : </b>Fozilatunnesa123@gmail.com</span></p>
+        <p class="card-text"><span class="las la-phone"></span> <span><b>Mobile : </b>018--------,017--------</span></p>
+       <p class="card-text"><span class="las la-location-arrow"></span> <span><b>Address : </b>Cokroborti, Jirani, Savar</span></p>
+
+        <button class="btn btn-primary">View doctors</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="col">
+    <div class="card">
+      <img src="img/h1.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Seikh fozilatunnesa Hospital </h5>
+       
+        <p class="card-text"><span class="las la-envelope"></span> <span><b>Email : </b>Fozilatunnesa123@gmail.com</span></p>
+        <p class="card-text"><span class="las la-phone"></span> <span><b>Mobile : </b>018--------,017--------</span></p>
+       <p class="card-text"><span class="las la-location-arrow"></span> <span><b>Address : </b>Cokroborti, Jirani, Savar</span></p>
+
+        <button class="btn btn-primary">View doctors</button>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="img/h1.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Seikh fozilatunnesa Hospital </h5>
+       
+        <p class="card-text"><span class="las la-envelope"></span> <span><b>Email : </b>Fozilatunnesa123@gmail.com</span></p>
+        <p class="card-text"><span class="las la-phone"></span> <span><b>Mobile : </b>018--------,017--------</span></p>
+       <p class="card-text"><span class="las la-location-arrow"></span> <span><b>Address : </b>Cokroborti, Jirani, Savar</span></p>
+
+        <button class="btn btn-primary">View doctors</button>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+
+</div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+<?php 
+  include "includes/footer.php"
+?>
---
+++ odabs/important.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=
+    
+    
+    , initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+
+<!-- Login form multi users -->
+
+<div class="input-div three">
+           		   <div class="i"> 
+           		    	<i class="fas fa-users"></i>
+           		   </div>
+
+                   <div class="div">
+           		    	<h5>Select : </h5>
+           		    	<select name="" id="">
+                               <option value="user">User</option>
+                               <option value="doctor">Doctor</option>
+                               <option value="admin">Admin</option>
+                           </select>
+            	  
+
+					</div>
+
+
+
+</body>
+</html>

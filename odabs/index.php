1d68c64835261180606984bcb2b3fd89a4ac1254
Md. Ariful Haque <70318840+Arifulhaque313@users.noreply.github.com>
2021-11-18 17:50:31 +0600

    Add files via upload

---
+++ odabs/index.php
@@ -0,0 +1,98 @@
+<?php 
+  include "includes/header.php"
+?>
+
+<!--Hero section-->
+
+<section class="hero container-fluid">
+
+    <div class="">
+        <div class="pt-5 text-center herotop">
+            <h1>Select Hospital , Find Doctor</h1>
+        </div>
+        <div class="text-center">
+            <h1>Get an appointment</h1>
+        </div>
+        <div class="text-center btndiv">
+            <button class="btn btn-success btn-lg"><b>Appointment </b> </button>
+        </div>
+    </div>
+
+
+</section>
+
+
+
+
+<section class="hero3 container my-5">
+
+    <div class="row">
+
+        <div class="col-md-6 col-12 pt-5">
+            <h1>Welcome to our System</h1>
+            <p>Our proposed project is an Online Doctor appointment booking system that provides any user or patients to
+                booking a doctor’s appointment easily in online. By using our system, you can easily book an appointment
+                at any hospital in our country. You will find doctors details according to the hospitals. The patients can book an
+                appointment in any hospital according to their choice or demands, also get their serial no and
+                appointment time You will find doctors details according to the hospital. Hence this system is best applicable system for easily find best doctors in
+                any hospitals and booking an appointment.</p>
+            <button class="btn btn-success">learn More</button>
+        </div>
+        <div class="col-md-6 col-12 text-center">
+
+            <img src="img/bg-doctor.png" alt="">
+        </div>
+    </div>
+
+</section>
+
+
+<!--Hero2 section-->
+
+<section class="hero2 container-fluid">
+
+    <div class="row">
+        <div class="col-lg-4 col-12 text-center text-white">
+            <h1>5+ Hospitals</h1>
+
+        </div>
+        <div class="col-lg-4 col-12 text-center text-white">
+            <h1>20+ Doctors</h1>
+
+        </div>
+        <div class="col-lg-4 col-12 text-center text-white">
+
+            <h1>30+ Users</h1>
+        </div>
+    </div>
+
+</section>
+
+
+
+<section class="hero3 container my-5">
+
+    <div class="row">
+
+        <div class="col-md-6 col-12 pt-5">
+            <h1>Welcome to our System</h1>
+            <p>Our proposed project is an Online Doctor appointment booking system that provides any user or patients to
+                booking a doctor’s appointment easily in online. By using our system, you can easily book an appointment
+                at any hospital in our country. You will find doctors details according to the hospitals. The patients can book an
+                appointment in any hospital according to their choice or demands, also get their serial no and
+                appointment time You will find doctors details according to the hospital. Hence this system is best applicable system for easily find best doctors in
+                any hospitals and booking an appointment. </p>
+            <button class="btn btn-success">learn More</button>
+        </div>
+        <div class="col-md-6 col-12 text-center">
+
+            <img src="img/bg-doctor.png" alt="">
+        </div>
+    </div>
+
+</section>
+
+
+<?php 
+  include "includes/footer.php"
+?>
